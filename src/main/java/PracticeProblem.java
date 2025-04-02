926885868bb1091d05092bbcac48019e4c66f1c9
Elliot Chan <elliot.chan@student.tdsb.on.ca>
2025-04-02 18:22:45 +0000

    finish practice problem

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,24 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int sumOfDigits(int n) {
+
+		//Base Case - when it stops
+			if(n < 0)
+				n*=-1;
+			if(n < 10)
+				return n;
+		//Recursive calls - what we repeat
+			return sumOfDigits(n / 10) + n % 10;
+		
+	}
+	public static int factorial(int n) {
+		if(n == 0)
+			return 1;
+		else if(n < 0)
+			return 0;
+		
+		return n * factorial(n-1);
+	}
 
 }
